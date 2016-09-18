e2c9353fc3041116b02e217aff8bcc4b919363ad
sanjanadodla <sanjana.rohan18@gmail.com>
2016-09-18 20:36:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
+var articles = {
+'article-one': {
     title: 'Article One: Sanjana Dodla',
     heading: 'Article One',
     date: 'Sep 18, 2016',
@@ -19,8 +20,25 @@ var articleOne = {
         <p>
             This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.
         </p>`
+    },
+'article-two': {
+    title: 'Article Two: Sanjana Dodla',
+    heading: 'Article Two',
+    date: 'Sep 19, 2016',
+    content: `<p>
+            This is the content for the second article
+        </p>`
+    },
+'article-three':{
+    title: 'Article Three: Sanjana Dodla',
+    heading: 'Article Three',
+    date: 'Sep 20, 2016',
+    content: `<p>
+            This is the content for the third article
+        </p>`
+    }
 };
-function createTemplate (data) {
+    function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
@@ -59,17 +77,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res){
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
